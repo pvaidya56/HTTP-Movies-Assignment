ab5467ef628273f7b3a370dfdd424f3e7f2107ab
Priya <pvaidya56@gmail.com>
2020-02-20 18:09:35 -0800

    added ability to update and delete stars of the movie

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-
+import UpdateMovie from './Movies/UpdateMovie'
 const App = () => {
   const [savedList, setSavedList] = useState([]);
 
@@ -21,6 +21,10 @@ const App = () => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
       />
+      <Route path="/update-movie/:id"
+      render={props => {
+        return <UpdateMovie { ...props } />
+      }} />
     </>
   );
 };
